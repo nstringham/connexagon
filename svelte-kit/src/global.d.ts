cf07e975b6f7f596500467f1dc207acffa66b984
Nate Stringham <44071655+nstringham@users.noreply.github.com>
2023-10-08 00:26:54 -0400

    build: ➕ add glsl to vite

---
+++ svelte-kit/src/global.d.ts
@@ -0,0 +1,9 @@
+declare module '*.vert' {
+	const shaderSource: string;
+	export default shaderSource;
+}
+
+declare module '*.frag' {
+	const shaderSource: string;
+	export default shaderSource;
+}
