ce7f423846f1d9bfcdd38c3514dcb8217a421ca1
Nate Stringham <44071655+nstringham@users.noreply.github.com>
2025-01-22 00:00:14 -0500

    feat: ✨ implement example supabase auth app
    https://supabase.com/docs/guides/auth/server-side/sveltekit

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,11 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+	let { data } = $props();
+	let { countries } = $derived(data);
+</script>
+
+<h1>Welcome to Supabase!</h1>
+<ul>
+	{#each countries as country}
+		<li>{country.name}</li>
+	{/each}
+</ul>
