fd9340cc9f80b78ae30f6648c451dadc5202952b
Nate Stringham <44071655+nstringham@users.noreply.github.com>
2025-01-23 21:44:07 -0500

    feat: ✨ only display users own games on home screen

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,11 +6,11 @@
 
 <h1>Welcome to Supabase!</h1>
 <ul>
-	{#each games as game}
+	{#each games as { game }}
 		<li>
 			<a href="/games/{game.id}">
 				{#each game.players as player}
-					<span style:color={player.color}>{player.profiles.name}</span>
+					<span style:color={player.color}>{player.profile.name}</span>
 				{/each}
 			</a>
 		</li>
--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -1,12 +1,34 @@
+import type { SupabaseClient, User } from "@supabase/supabase-js";
 import type { PageLoad } from "./$types";
+import type { Database } from "$lib/database-types";
 
 export const load: PageLoad = async ({ parent }) => {
-	const { supabase } = await parent();
-	const { data: games } = await supabase
-		.from("games")
-		.select(
-			"id, turn, winner_player_number, players(user_id, player_number, color, profiles(name))",
-		)
-		.order("id");
-	return { games: games ?? [] };
+	const { supabase, user } = await parent();
+	return { games: await getGamesForUser(supabase, user) };
 };
+
+async function getGamesForUser(supabase: SupabaseClient<Database>, user: User | null) {
+	if (user == null) {
+		return [];
+	}
+
+	const { data: games, error } = await supabase
+		.from("players")
+		.select(
+			`
+				game:games(
+					id,
+					turn,
+					winner_player_number,
+					players(user_id, player_number, color, profile:profiles(name))
+				)
+			`,
+		)
+		.eq("user_id", user?.id);
+
+	if (error) {
+		console.error(error);
+	}
+
+	return games ?? [];
+}
