40e464e540961ab48f21fab51bbf74451fe20fc3
Nate Stringham <44071655+nstringham@users.noreply.github.com>
2025-01-28 09:29:41 -0500

    feat: 🗃️ create turns table

---
+++ supabase/migrations/20250128142717_create_turns_table.sql
@@ -0,0 +1,23 @@
+CREATE TABLE IF NOT EXISTS public.turns (
+	game_id public.xid NOT NULL,
+	turn_number smallint NOT NULL,
+	cells smallint[] NOT NULL,
+	created_at timestamp with time zone DEFAULT now() NOT NULL
+);
+
+ALTER TABLE public.turns OWNER TO postgres;
+
+ALTER TABLE ONLY public.turns
+	ADD CONSTRAINT turns_pkey PRIMARY KEY (game_id, turn_number);
+
+ALTER TABLE ONLY public.turns
+	ADD CONSTRAINT turns_game_id_fkey FOREIGN KEY (game_id) REFERENCES public.games (id);
+
+ALTER TABLE public.turns ENABLE ROW LEVEL SECURITY;
+
+GRANT ALL ON TABLE public.turns TO anon;
+
+GRANT ALL ON TABLE public.turns TO authenticated;
+
+GRANT ALL ON TABLE public.turns TO service_role;
+
