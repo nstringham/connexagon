0701b1603ea4c57fb817dc137e8f6eb03f9343c2
Nate Stringham <44071655+nstringham@users.noreply.github.com>
2025-01-29 14:50:46 -0500

    feat: 🗃️ add rules to the name column on the profiles table

---
+++ supabase/migrations/20250129190857_require_profile_name.sql
@@ -0,0 +1,9 @@
+alter table public.profiles
+alter column name
+set not null;
+
+alter table public.profiles
+add constraint name_regexp check (name ~* '^[a-z][a-z0-9_ ]+[a-z0-9]$');
+
+alter table public.profiles
+add constraint name_unique unique (name);
