44bc0b9c44af027a642205ea82110aaab15f73e4
Nate Stringham <44071655+nstringham@users.noreply.github.com>
2025-01-29 21:35:17 -0500

    refactor: ♻️ use supabase to upsert profile

--- src/routes/account/+page.svelte
+++ src/routes/account/+page.svelte
@@ -2,7 +2,7 @@
 	import { invalidateAll } from "$app/navigation";
 
 	const { data } = $props();
-	const { profile, supabase } = $derived(data);
+	const { profile, supabase, user } = $derived(data);
 
 	let name = $state("");
 
@@ -13,7 +13,10 @@
 	});
 
 	async function save() {
-		const { error } = await supabase.rpc("set_name", { name });
+		if (user == null) {
+			throw Error("cannot set name for non-existent user");
+		}
+		const { error } = await supabase.from("profiles").upsert({ user_id: user.id, name });
 		if (error) {
 			throw error;
 		}
--- supabase/migrations/20250130012942_add_function_to_change_name.sql
+++
@@ -1,9 +0,0 @@
-create or replace function public.set_name (name text) returns void language sql as $$
-	insert into
-		public.profiles (user_id, name)
-	values
-		(auth.uid (), name)
-	on conflict (user_id) do update
-	set
-		name = excluded.name;
-$$;
