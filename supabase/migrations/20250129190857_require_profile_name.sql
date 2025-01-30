cc6e3749565b049f1857abb9c073116204573081
Nate Stringham <44071655+nstringham@users.noreply.github.com>
2025-01-30 09:48:33 -0500

    refactor: ♻️ rename constraint for consistency

--- supabase/migrations/20250129190857_require_profile_name.sql
+++ supabase/migrations/20250129190857_require_profile_name.sql
@@ -6,4 +6,4 @@ alter table public.profiles
 add constraint name_regexp check (name ~* '^[a-z][a-z0-9_ ]+[a-z0-9]$');
 
 alter table public.profiles
-add constraint name_unique unique (name);
+add constraint unique_name unique (name);
