bb9e730a9146818865b6dac1f8d60b2949634e12
Nate Stringham <44071655+nstringham@users.noreply.github.com>
2025-02-01 15:34:20 -0500

    feat: ✨ precompute triangle numbers

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,24 @@
 import { defineConfig } from "vitest/config";
 import { sveltekit } from "@sveltejs/kit/vite";
+import virtual from "vite-plugin-virtual";
 
 export default defineConfig({
-	plugins: [sveltekit()],
+	plugins: [
+		sveltekit(),
+		virtual({
+			"virtual:triangle-numbers": [...getTriangleNumbers()],
+		}),
+	],
 
 	test: {
 		include: ["src/**/*.{test,spec}.{js,ts}"],
 	},
 });
+
+function* getTriangleNumbers() {
+	let total = 0;
+	for (let i = 0; i < 7 + 7; i++) {
+		total += i;
+		yield total;
+	}
+}
