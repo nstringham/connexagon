b99aee3004427178bc1bde122b4722c9e751e74d
Nate Stringham <44071655+nstringham@users.noreply.github.com>
2025-02-04 15:04:19 -0500

    chore: 🔧 apply stylelint to svelte files

--- stylelint.config.js
+++ stylelint.config.js
@@ -3,4 +3,13 @@
 /** @type {import('stylelint').Config} */
 export default {
 	extends: ["stylelint-config-standard"],
+	overrides: [
+		{
+			files: ["**/*.svelte"],
+			customSyntax: "postcss-html",
+			rules: {
+				"selector-pseudo-class-no-unknown": [true, { ignorePseudoClasses: ["global"] }],
+			},
+		},
+	],
 };
