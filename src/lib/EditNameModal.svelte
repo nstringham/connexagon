a147b3058974000cc246fc8978221ce631c700f5
Nate Stringham <44071655+nstringham@users.noreply.github.com>
2025-02-12 14:35:20 -0500

    feat: 🦺 add client side validation for name

--- src/lib/EditNameModal.svelte
+++ src/lib/EditNameModal.svelte
@@ -27,7 +27,46 @@
     }
   });
 
-  async function save() {
+  function getValidationError(name: string): string | null {
+    if (!name) {
+      return "Name is required";
+    }
+
+    if (name.length < 3) {
+      return "Name must be at least 3 characters long";
+    }
+
+    if (name.length > 12) {
+      return "Name cannot be longer than 12 characters";
+    }
+
+    if (!/^[a-z0-9_ ]+$/i.test(name)) {
+      return "Name can only contain letters, number, spaces, and underscores";
+    }
+
+    if (!/^[a-z]/i.test(name)) {
+      return "Name must start with a letter";
+    }
+
+    if (!/[a-z0-9]$/i.test(name)) {
+      return "Name must end with a letter or number";
+    }
+
+    return null;
+  }
+
+  const validationError = $derived(getValidationError(name));
+
+  let dirty = $state(false);
+
+  $effect(() => {
+    if (name != "") {
+      dirty = true;
+    }
+  });
+
+  async function save(event: SubmitEvent) {
+    event.preventDefault();
     const { error } = await supabase.from("profiles").upsert({ user_id: user.id, name });
     if (error) {
       throw error;
@@ -38,7 +77,31 @@
 </script>
 
 <Modal bind:open preventCancel={profile == null}>
-  <label>Name: <input type="text" name="name" bind:value={name} /></label>
+  <form onsubmit={save}>
+    <label>
+      Name:<br />
+      <input
+        type="text"
+        name="name"
+        bind:value={name}
+        required
+        minlength="3"
+        maxlength="12"
+        pattern="[a-zA-Z][a-zA-Z0-9_ ]+[a-zA-Z0-9]"
+      />
+      {#if dirty && validationError != null}
+        <p class="error">{validationError}</p>
+      {/if}
+    </label>
 
-  <button onclick={save}>Save</button>
+    <div>
+      <button type="submit" disabled={validationError != null}>Save</button>
+    </div>
+  </form>
 </Modal>
+
+<style>
+  .error {
+    color: red;
+  }
+</style>
