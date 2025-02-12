54ceb11d994f1f1ae3c172c8c6335d425f6a7785
Nate Stringham <44071655+nstringham@users.noreply.github.com>
2025-02-11 21:16:02 -0500

    refactor: ♻️ use invalidate instead of invalidate all for all auth events but  do not await it to avoid sign out bug

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
   import "modern-normalize";
   import "$lib/themes.css";
-  import { invalidate, invalidateAll } from "$app/navigation";
+  import { invalidate } from "$app/navigation";
   import { PUBLIC_SUPABASE_URL } from "$env/static/public";
   import SignInModal, { openSignInModal } from "$lib/SignInModal.svelte";
   import { onMount } from "svelte";
@@ -10,11 +10,9 @@
   let { supabase, session, user } = $derived(data);
 
   onMount(() => {
-    const { data } = supabase.auth.onAuthStateChange(async (event, newSession) => {
-      if (event === "SIGNED_OUT") {
-        invalidateAll();
-      } else if (newSession?.expires_at !== session?.expires_at) {
-        await invalidate("supabase:auth");
+    const { data } = supabase.auth.onAuthStateChange((_, newSession) => {
+      if (newSession?.expires_at !== session?.expires_at) {
+        invalidate("supabase:auth");
       }
     });
 
