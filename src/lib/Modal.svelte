566a586e40d3ba7900f4d42a6e33ccaf95cc6c3d
Nate Stringham <44071655+nstringham@users.noreply.github.com>
2025-02-13 13:10:34 -0500

    feat: ✨ allow user to close modal by clicking backdrop

--- src/lib/Modal.svelte
+++ src/lib/Modal.svelte
@@ -22,6 +22,8 @@
   });
 </script>
 
+<!-- svelte-ignore a11y_click_events_have_key_events -->
+<!-- svelte-ignore a11y_no_noninteractive_element_interactions -->
 <dialog
   bind:this={dialogElement}
   oncancel={(event) => {
@@ -31,6 +33,22 @@
   }}
   onclose={() => {
     open = false;
+  }}
+  onclick={(event) => {
+    if (preventCancel) {
+      return;
+    }
+
+    const rect = event.currentTarget.getBoundingClientRect();
+
+    if (
+      rect.left > event.clientX ||
+      rect.right < event.clientX ||
+      rect.top > event.clientY ||
+      rect.bottom < event.clientY
+    ) {
+      dialogElement.close();
+    }
   }}
 >
   {@render children()}
