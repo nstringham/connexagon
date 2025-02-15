4672cc996f735329e40156b42a240c3e0d217bc1
Nate Stringham <44071655+nstringham@users.noreply.github.com>
2025-02-15 18:07:41 -0500

    refactor: 🔥 remove unused type

--- src/lib/modal.ts
+++ src/lib/modal.ts
@@ -1,11 +1,6 @@
 import { mount, unmount, type Snippet } from "svelte";
 import Modal from "./Modal.svelte";
 
-export type ModalHandel<T = void> = {
-  returnValue: Promise<T>;
-  close(): void;
-};
-
 export function showModal(children: Snippet, { preventCancel }: { preventCancel?: boolean } = {}) {
   return new Promise<void>((resolve) => {
     // eslint-disable-next-line @typescript-eslint/no-unsafe-argument -- this is ok because we have no exports
