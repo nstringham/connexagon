cf3378bbaa5f30340b2d092d61b645d1d5e4c27d
Nate Stringham <44071655+nstringham@users.noreply.github.com>
2025-02-15 23:44:32 -0500

    feat: ✨ adjust theme color based on prefers-color-scheme

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,6 +1,14 @@
 import type { LayoutServerLoad } from "./$types";
 
-export const load: LayoutServerLoad = async ({ locals: { safeGetSession }, cookies }) => {
+export const load: LayoutServerLoad = async ({
+  locals: { safeGetSession },
+  cookies,
+  setHeaders,
+}) => {
+  setHeaders({
+    "Accept-CH": "Sec-CH-Prefers-Color-Scheme",
+  });
+
   const { session } = await safeGetSession();
   return {
     session,
--- src/routes/manifest/+server.ts
+++ src/routes/manifest/+server.ts
@@ -4,27 +4,34 @@ import type { RequestHandler } from "./$types";
 import anySvg from "$lib/logo/icon.svg";
 import maskableSvg from "$lib/logo/maskable-icon.svg";
 
-const manifest: WebAppManifest = {
-  name: "Connexagon",
-  start_url: "/",
-  display: "standalone",
-  theme_color: "#00cdd0",
-  icons: [
-    {
-      src: anySvg,
-      type: "image/svg+xml",
-      sizes: "any",
-      purpose: "any",
-    },
-    {
-      src: maskableSvg,
-      type: "image/svg+xml",
-      sizes: "any",
-      purpose: "maskable",
-    },
-  ],
-};
+export const GET: RequestHandler = ({ request, setHeaders }) => {
+  setHeaders({
+    Vary: "Sec-CH-Prefers-Color-Scheme",
+  });
 
-export const GET: RequestHandler = () => {
+  const prefersLight = request.headers.get("Sec-CH-Prefers-Color-Scheme") == "light";
+
+  const manifest: WebAppManifest = {
+    name: "Connexagon",
+    start_url: "/",
+    display: "standalone",
+    theme_color: prefersLight ? "#ffffff" : "#121212",
+    background_color: prefersLight ? "#ffffff" : "#121212",
+    icons: [
+      {
+        src: anySvg,
+        type: "image/svg+xml",
+        sizes: "any",
+        purpose: "any",
+      },
+      {
+        src: maskableSvg,
+        type: "image/svg+xml",
+        sizes: "any",
+        purpose: "maskable",
+      },
+    ],
+  };
+
   return new Response(JSON.stringify(manifest));
 };
