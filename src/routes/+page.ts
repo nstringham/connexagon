c5fa69f02f5ed994a51a4369b417c6021750792f
Nate Stringham <44071655+nstringham@users.noreply.github.com>
2025-02-17 16:21:43 -0500

    feat: ✨ sort and group games into sections

--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -7,14 +7,17 @@ export const load: PageLoad = async ({ parent }) => {
     return { games: [] };
   }
 
-  const { data: games, error } = await supabase
+  const { data, error } = await supabase
     .from("players")
     .select(
       `
+        created_at,
         game:games(
           id,
           turn,
           winner,
+          started_at,
+          completed_at,
           players(user_id, turn_order, color, profile:profiles(name))
         )
       `,
@@ -25,5 +28,37 @@ export const load: PageLoad = async ({ parent }) => {
     console.error(error);
   }
 
-  return { games: games ?? [] };
+  const games = data?.map((row) => ({ ...row.game, joined_at: row.created_at })) ?? [];
+
+  for (const game of games) {
+    if (game.started_at != null) {
+      game.players.sort((a, b) => a.turn_order! - b.turn_order!);
+    }
+  }
+
+  const { pending, yourTurn, current, finished } = Object.groupBy(games, (game) => {
+    if (game.turn == null) {
+      return "pending";
+    } else if (game.completed_at != null) {
+      return "finished";
+    } else if (game.players[game.turn % game.players.length].user_id == user.id) {
+      return "yourTurn";
+    } else {
+      return "current";
+    }
+  });
+
+  pending?.sort((a, b) => (a.joined_at < b.joined_at ? 1 : -1));
+  yourTurn?.sort((a, b) => (a.started_at! < b.started_at! ? 1 : -1));
+  current?.sort((a, b) => (a.started_at! < b.started_at! ? 1 : -1));
+  finished?.sort((a, b) => (a.completed_at! < b.completed_at! ? 1 : -1));
+
+  return {
+    games: [
+      { name: "Pending Games", games: pending },
+      { name: "Your Turn", games: yourTurn },
+      { name: "Current Games", games: current },
+      { name: "Finished Games", games: finished },
+    ].filter((group) => group.games != undefined && group.games.length > 0),
+  };
 };
