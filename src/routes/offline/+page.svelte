d014b8c741116c0737d7c99ebe41902f46cb2453
Nate Stringham <44071655+nstringham@users.noreply.github.com>
2025-02-18 17:01:03 -0500

    feat: ✨ add offline fallback page

---
+++ src/routes/offline/+page.svelte
@@ -0,0 +1,36 @@
+<svelte:head>
+  <script type="module">
+    if (location.pathname.startsWith("/offline")) {
+      location.href = "/";
+    }
+
+    const title = document.querySelector("#title");
+    const description = document.querySelector("#description");
+
+    if (!navigator.onLine) {
+      title.innerHTML = "You are offline!";
+      description.innerHTML = "Please connect to the internet to play Connexagon.";
+      window.addEventListener("online", () => setTimeout(location.reload()));
+    }
+  </script>
+
+  <style>
+    .offline-grid {
+      margin: 0;
+      min-height: 100svh;
+      display: grid;
+      place-content: center;
+      justify-items: center;
+    }
+
+    header {
+      display: none !important;
+    }
+  </style>
+</svelte:head>
+
+<form class="offline-grid">
+  <h1 id="title">Cannot connect to server!</h1>
+  <p id="description">Check your internet connection and try again.</p>
+  <button type="submit">Try Again</button>
+</form>
--- src/service-worker.ts
+++ src/service-worker.ts
@@ -2,11 +2,59 @@
 /// <reference no-default-lib="true"/>
 /// <reference lib="esnext" />
 /// <reference lib="webworker" />
-
-import {} from "$service-worker";
-
 declare const self: ServiceWorkerGlobalScope;
 
-self.addEventListener("fetch", () => {
-  // do nothing
+import { version } from "$service-worker";
+
+const CACHE = `cache-${version}`;
+
+self.addEventListener("install", (event) => {
+  async function addOfflinePageToCache() {
+    const cache = await caches.open(CACHE);
+    await cache.add(new Request("/offline"));
+  }
+
+  event.waitUntil(addOfflinePageToCache());
+  void self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  async function enableNavigationPreload() {
+    await self.registration.navigationPreload.enable();
+  }
+
+  async function deleteOldCaches() {
+    for (const key of await caches.keys()) {
+      if (key !== CACHE) {
+        await caches.delete(key);
+      }
+    }
+  }
+
+  event.waitUntil(enableNavigationPreload());
+  event.waitUntil(deleteOldCaches());
+  void self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+  if (event.request.mode === "navigate") {
+    async function respond() {
+      try {
+        const preloadResponse = (await event.preloadResponse) as Response | undefined;
+        if (preloadResponse) {
+          return preloadResponse;
+        }
+
+        const networkResponse = await fetch(event.request);
+        return networkResponse;
+      } catch (error) {
+        console.log("Fetch failed; returning offline page instead.", error);
+
+        const cache = await caches.open(CACHE);
+        return (await cache.match("/offline"))!;
+      }
+    }
+
+    event.respondWith(respond());
+  }
 });
