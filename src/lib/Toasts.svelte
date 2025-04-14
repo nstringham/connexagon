8a494f653131b54639180df5bbb25234a6d81606
Nate Stringham <44071655+nstringham@users.noreply.github.com>
2025-04-14 15:13:57 -0400

    feat: ✨ add styling to toasts

--- src/lib/Toasts.svelte
+++ src/lib/Toasts.svelte
@@ -4,38 +4,69 @@
     style?: "error";
   };
 
-  let toasts: (Toast & { expiration: number })[] = $state([]);
-
   export function showToast(toast: Toast) {
-    const expiration = Date.now() + 5_000;
+    toastQueue.push(toast);
+    mainLoop();
+  }
 
-    toasts.push({ ...toast, expiration });
-    setTimeout(() => {
-      toasts = toasts.filter((toast) => toast.expiration > Date.now());
-    }, 5_000);
+  let toastQueue: Toast[] = [];
+  let currentToast: Toast | undefined = $state();
+
+  let active = false;
+  async function mainLoop() {
+    if (active) {
+      return;
+    }
+
+    active = true;
+
+    while (toastQueue.length > 0) {
+      currentToast = toastQueue.shift();
+      await wait(5_000);
+
+      currentToast = undefined;
+      await wait(1_000);
+    }
+
+    active = false;
+  }
+
+  function wait(ms: number): Promise<void> {
+    return new Promise((resolve) => setTimeout(resolve, ms));
   }
 </script>
 
 <script lang="ts">
   import "$lib/hexagons.css";
+  import { fade, fly } from "svelte/transition";
 </script>
 
-<div class="wrapper">
-  {#each toasts as toast, i (i)}
-    <div class="toast {toast.style}">
-      <div class="background clip-hexagon"></div>
-      <span class="message">{toast.message}</span>
-    </div>
-  {/each}
-</div>
+{#if currentToast != undefined}
+  <div class="toast clip-hexagon {currentToast.style}" in:fly={{ y: "100%" }} out:fade>
+    <div class="background clip-hexagon"></div>
+    <span class="message">{currentToast.message}</span>
+  </div>
+{/if}
 
 <style>
-  .wrapper {
+  .toast {
+    color: var(--background-color);
+    background-color: var(--foreground-color);
+    font-size: 14pt;
+    font-weight: 400;
     position: fixed;
     bottom: 0;
+    margin: 24px;
+    height: var(--height);
+    padding: 0 calc(var(--height) / 2);
+    display: grid;
+    place-content: center;
+
+    --height: 48px;
   }
 
   .toast.error {
-    color: red;
+    background-color: var(--red);
+    color: white;
   }
 </style>
